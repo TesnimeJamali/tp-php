ac68cdae88cac97d1d1d120c27f33c8bed448fc4
MariemOuertani <mariem.ouertani@insat.ucar.tn>
2025-03-29 11:06:36 +0100

    second commit
    After reading and well understading the first commit , i've worked on the ssecond to add the refresh button and other details.

--- exercice_2/index.php
+++ exercice_2/index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gestion des sessions </title>
+    <link rel="stylesheet" href="exercice2.css">
 </head>
 <body>
 <?php
@@ -11,13 +12,26 @@ require 'session.php';
 $session = Session::getInstance();
 if (!$session->has('nbvisits')) {
     $session->set('nbvisits', 1);
-    echo "<br>Bienvenue à notre platforme";
+    echo "<br> <h1> Bienvenue à notre platforme </h1>";
 } else {
     $nbVisits = $session->get('nbvisits');
     $session->set('nbvisits', $nbVisits + 1);  // Incrémente et met à jour
-    echo "<br>Merci pour votre fidélité, c'est votre " . $session->get('nbvisits') . "ème visite.";
+    echo "  <h1> <br>Merci pour votre fidélité, c'est votre " . $session->get('nbvisits') . "ème visite. </h1>
+    ";
+}
+if (isset($_POST['reset_session'])) {
+    $session->destroy(); // Détruit la session
+    echo "<h1> La session a été réinitialisée </h1>";
+    header("Location: index.php"); // Redirect pour réinitialiser l'affichage
+    exit();
+
 }
 ?>
+<div>
+<form method="post">
+            <button type="submit" name="reset_session" class="reset-button">Réinitialiser la session</button>
+        </form>
+    </div>
 </body>
 </html>
 
