ff7f8e70a95a5b9538c8c1b5495cd6ac3b3dbfe2
MariemOuertani <mariem.ouertani@insat.ucar.tn>
2025-03-30 11:37:09 +0100

    lastUpdate students.php

--- exercice_4/students.php
+++ exercice_4/students.php
@@ -20,6 +20,7 @@ $results = $query->fetchAll(PDO::FETCH_OBJ);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des Étudiants</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
@@ -40,7 +41,7 @@ $results = $query->fetchAll(PDO::FETCH_OBJ);
                         <td><?php echo htmlspecialchars($student->id); ?></td>
                         <td><?php echo htmlspecialchars($student->name); ?></td>
                         <td><?php echo htmlspecialchars($student->birth_date); ?></td>
-                        <td><a href="detailEtudiant.php?id=<?php echo $student->id; ?>" class="btn btn-info">Détails</a></td>
+                        <td><a href="detailEtudiant.php?id=<?php echo $student->id; ?>" class="btn btn-info">Détails </a></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
