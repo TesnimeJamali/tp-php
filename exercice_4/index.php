2e990fa58d669c0521da59339f9f6ee3183e1cfd
MariemOuertani <mariem.ouertani@insat.ucar.tn>
2025-03-30 11:36:16 +0100

    lastUpdate index.php

--- exercice_4/index.php
+++ exercice_4/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion à la Base de Données</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href=style.css rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
