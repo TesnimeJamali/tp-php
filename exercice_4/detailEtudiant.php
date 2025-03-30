8f0d4000e10c4e5fe99ceb20447711ef6f0604c3
MariemOuertani <mariem.ouertani@insat.ucar.tn>
2025-03-30 11:35:44 +0100

    lastUpdate detailEtudiant.php

--- exercice_4/detailEtudiant.php
+++ exercice_4/detailEtudiant.php
@@ -30,6 +30,7 @@ if ($studentId > 0) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Détails de l'Étudiant</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+            <link href="style.css" rel="stylesheet">
         </head>
         <body>
         <div class="container mt-5">
